aced7f63fa56b79489935fa7900fd30b679f9b68
jungu0416 <junguYY0809@gmail.com>
2022-03-18 14:03:35 +0900

    bona

---
+++ assets/js/bona.js
@@ -0,0 +1,15 @@
+/*bona javascript
+*
+*
+*
+*/
+var btn_click_about_bona = function(){
+    alert("TEST");
+}
+
+
+var footer_button = document.getElementById("bona-footer");
+    footer_button.onclick = btn_click_about_bona;
+
+
+
