4142dd700a92b56a39106197f1bdf2a3801d19b6
jungu <jungu>
2022-09-05 21:07:00 +0900

    mobile & web

--- assets/js/bona.js
+++ assets/js/bona.js
@@ -3,13 +3,83 @@
 *
 *
 */
-var btn_click_about_bona = function(){
-    alert("TEST");
+
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+
+    cssChange();
+});
+
+let cssChange = () => {
+
+    if(!isMobile()){
+        let cssStyle = document.getElementsByClassName('bona-img-width');
+        debugger;
+        for(let i=0; i< cssStyle.length; i++){
+            cssStyle[i].style.width="70%";
+        }
+    }
+
+}
+
+const isMobile = () => {
+    const user = navigator.userAgent;
+    let isCheck = false;
+
+    if ( user.indexOf("iPhone") > -1 || user.indexOf("Android") > -1 ) {
+        isCheck = true;
+    }
+
+    return isCheck;
 }
 
 
-var footer_button = document.getElementById("bona-footer");
-    footer_button.onclick = btn_click_about_bona;
 
+let submit = () => {
+    
+    if(validate()) {
+        alert('정상처리 되었습니다. \n빠른 시일내에 답변 드리겠습니다.');
+        location.href = "bona_main.html";
+    }
+}
+
+let validate = () => {
+    let name = document.getElementById('name');
+    let subject = document.getElementById('subject');
+    let email = document.getElementById('email');
+    let message = document.getElementById('message');
+
+    if(name.value == ''){
+        alert('닉네임을 입력해주세요');
+        return false;
+    }
+    if(subject.value == ''){
+        alert('제목을 입력해주세요');
+        return false;
+    }
+    if(email.value == ''){
+        alert('이메일 입력해주세요');
+        return false;
+    }
+    if(message.value == ''){
+        alert('내용 입력해주세요');
+        return false;
+    }
+
+    return true;
+}
 
 
