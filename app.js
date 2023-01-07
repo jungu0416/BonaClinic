5c2344a633cc92a53461c01319d3b2ebd0e9778b
DESKTOP-I9F93C9\Jungu <jungu0416@naver.com>
2023-01-07 09:44:49 +0900

    node js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중')
+})
